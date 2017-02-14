7512b480a5bac7463b7228eab01a3ea69f913f1d
LaurentGirard <laurent.girard44620@gmail.com>
2017-02-14 13:42:38 +0100

    add mongodb to apt-get install

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -x
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y git
+sudo apt-get install -y git mongodb
 
 # Download DevStack
 git clone https://git.openstack.org/openstack-dev/devstack /devstack --single-branch
